817be100be0e6b65b7a2ff8972167d02217f4d87
Javier Tinoco Diaz <jtinocof@yahoo.com>
2019-12-29 19:29:34 -0600

    Initial Commit

---
+++ anothergithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  anothergithub
+//
+//  Created by Javier Tinoco Diaz on 29/12/19.
+//  Copyright © 2019 AppTinoco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
