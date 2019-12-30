23557e458d0f8ff37976dde8f4c4330277fd7f9b
Javier Tinoco Diaz <jtinocof@yahoo.com>
2019-12-29 19:34:36 -0600

    second commit test

--- anothergithub/ViewController.swift
+++ anothergithub/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("add to github")
+        print("second commit to github..")
     }
 
 
